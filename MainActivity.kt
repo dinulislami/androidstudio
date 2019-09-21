3ec664f9ff434265ae97c4fe55f4f122c634fa62
dinulislami <55618712+dinulislami@users.noreply.github.com>
2019-09-21 15:33:05 +0700

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project1_5170411225
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
